4e46ee24c34e93d7091e18e47903e0a116365154
Vinicius Pinheiro <vinicius.gdpinheiro@gmail.com>
2026-05-05 09:42:17 -0300

    arquivos de conexão MQTT

---
+++ backend/app/main.py
@@ -0,0 +1,91 @@
+from time import sleep
+from umqtt import MQTTClient
+import ubinascii
+import machine
+import network
+import onewire, ds18x20, os
+
+
+def cbTrataMsg(topic, msg):
+    print(f'Msg recebida no tópico: {topic.decode("utf-8")}')
+    print(msg.decode('utf-8'))
+
+
+def carregar_config():
+    conf = {}
+    try:
+        with open(".env", "r") as f:
+            for linha in f:
+                linha = linha.strip()
+                if "=" in linha and not linha.startswith("#"):
+                    chave, valor = linha.split("=", 1)
+                    conf[chave.strip()] = valor.strip()
+        return conf
+    except Exception as e:
+        print("Erro ao ler .env:", e)
+        return None
+
+
+# --- Pegando informações .env ---
+config = carregar_config()
+if config:
+    WIFI_SSID = config.get('WIFI_SSID')
+    WIFI_PWD  = config.get('WIFI_PWD')
+    MQTT_SERVER = config.get('MQTT_SERVER')
+    MQTT_PORT   = int(config.get('MQTT_PORT', 8883)) 
+    MQTT_USER   = config.get('MQTT_USER').encode()  
+    MQTT_PWD    = config.get('MQTT_PWD').encode()    
+    TOPIC_PUB   = config.get('MQTT_TOPIC').encode()  
+else:
+    print(".env não encontrado ou vazio!")
+
+
+# --- Conectando com a Rede ---
+rede = network.WLAN(network.STA_IF)
+rede.active(True)
+rede.connect(WIFI_SSID, WIFI_PWD)
+while not rede.isconnected():
+  print(".", end="")
+  sleep(0.5)
+print("\nConectado em", rede.ifconfig()[0])
+
+
+# --- Configurando MQTT ---
+print("Inicializando conexão com o broker...")
+client_id = ubinascii.hexlify(machine.unique_id())
+
+try:
+    client = MQTTClient(
+        client_id, 
+        MQTT_SERVER,
+        port=MQTT_PORT,
+        user=MQTT_USER,
+        password=MQTT_PWD,
+        ssl=True,
+        ssl_params={'server_hostname': MQTT_SERVER} 
+    )
+    client.set_callback(cbTrataMsg)
+    client.connect()
+    client.subscribe(TOPIC_PUB)
+    print("MQTT Conectado!")
+except OSError as e:
+    print("Erro ao conectar no MQTT:", e)
+    sleep(2)
+    machine.reset()
+
+
+# --- Loop ---
+while True:
+    try:
+        client.check_msg()
+        valor_temp = "Joao Bobo"
+        msg_json = '{"msg": "' + valor_temp + '"}' 
+        print(f"Publicando: {msg_json}")
+        client.publish(TOPIC_PUB, msg_json)
+        sleep(5) 
+        
+    except OSError as e:
+        print("Erro de conexão detectado:", e)
+        sleep(5)
+        machine.reset()
+        
