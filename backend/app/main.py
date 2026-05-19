ff40a6d8edc5df819377bcc0c73259948cc61ede
Vinicius Pinheiro <vinicius.gdpinheiro@gmail.com>
2026-05-19 09:53:07 -0300

    sensor infravermelho implementado na main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
 from time import sleep
 from umqtt import MQTTClient
+from machine import Pin
 import ubinascii
 import machine
 import network
+import time, math
+import ujson
 import onewire, ds18x20, os
 
 
@@ -75,14 +78,47 @@ except OSError as e:
 
 
 # --- Loop ---
+sensor1 = Pin(18, Pin.IN)
+sensor2 = Pin(4, Pin.IN)
+tempo1, tempo2 = 0, 0
+distancia = 0.93
+raio = 0.12
+
+def velocidade(dist, temp1, temp2):
+    dif_ms = time.ticks_diff(temp2, temp1)
+    dif_segundos = dif_ms / 1000
+
+    if dif_segundos <= 0:
+        return 0
+    
+    velocidade_ms = dist / dif_segundos
+    velocidade_kmh = velocidade_ms * 3.6
+    return velocidade_kmh
+
 while True:
     try:
         client.check_msg()
-        valor_temp = "Joao Bobo"
-        msg_json = '{"msg": "' + valor_temp + '"}' 
-        print(f"Publicando: {msg_json}")
-        client.publish(TOPIC_PUB, msg_json)
-        sleep(5) 
+
+        if (tempo1 != 0 and tempo2 != 0) or (
+            tempo1 != 0 and (time.ticks_diff(time.ticks_ms(), tempo1)/1000) > (2 * math.pi * math.sqrt(raio / 9.8)) + 2):
+
+            msg_json = {
+                "sensor1" : tempo1,
+                "sensor2" : tempo2,
+                "distancia" : distancia,
+                "velocidade" : velocidade(distancia, tempo1, tempo2)
+            }
+            print(f"Publicando: {msg_json}")
+            client.publish(TOPIC_PUB, ujson.dumps(msg_json))
+            tempo1, tempo2 = 0, 0
+
+        if sensor1.value() == 0 and tempo1 == 0:
+            tempo1 = time.ticks_ms() 
+            print("Sensor 1 ativado!")
+
+        if sensor2.value() == 0 and tempo2 == 0:
+            tempo2 = time.ticks_ms() 
+            print("Sensor 2 ativado!")
         
     except OSError as e:
         print("Erro de conexão detectado:", e)
